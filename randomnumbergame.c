90cbf61c9e2b8f1c232f27e68de4b55fd2cb6f44
harmandeepsingh2025 <harmandeepsingh7814@gmail.com>
2025-09-08 18:33:57 +0530

    final version

--- randomnumbergame.c
+++ randomnumbergame.c
@@ -1,35 +1,39 @@
-//random number generator code
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+// random number generator code
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void main()
 {
   srand(time(0));
-  int random_number = (rand() % 100)+1;
+  int random_number = (rand() % 100) + 1;
 
-  //game code
+  // game code
 
-  int guessed_number ;
-  int numberofgusses=0;
-do{
-   printf("enter number:");
-   scanf("%d",&guessed_number);
-     if (scanf("%d", &guessed_number) != 1) {
-            printf("Invalid input! Please enter a number.\n");
-     }
-   if(guessed_number>random_number){
-     printf("wrong! enter lower number\n");}
-  else if (guessed_number<random_number){
-     printf("wrong! enter higher number\n");}
-  else if(scanf("%d", &guessed_number) != 1){
+  int guessed_number;
+  int numberofgusses = 0;
+  do
+  {
+    printf("enter number:");
+    if (scanf("%d", &guessed_number) != 1)
+    {
       printf("Invalid input! Please enter a number.\n");
-  }
-  else 
-     printf("congo!!");
-    numberofgusses++;     
-    } while (guessed_number != random_number);
+      while (getchar() != '\n');
+      continue;
+    }
+    if (guessed_number > random_number)
+    {
+      printf("wrong! enter lower number\n");
+    }
+    else if (guessed_number < random_number)
+    {
+      printf("wrong! enter higher number\n");
+    }
 
-printf("you guess it right!! number is %d in %d trys",random_number,numberofgusses);
+    else
+      printf("congo!!");
+    numberofgusses++;
+  } while (guessed_number != random_number);
+
+  printf("you guess it right!! number is %d in %d trys", random_number, numberofgusses);
 }
-
